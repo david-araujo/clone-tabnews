15506f833ddaa64a74294baa2b9d3213014732e5
David Araujo <david.araujo91@gmail.com>
2023-08-11 18:58:43 +0000

    add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do TabNews.com.br from scratch 😁
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1>Iniciando com alegria mais um dia para aprender!!!</h1>
+function Home() {
+  return <h1>Iniciando com alegria mais um dia para aprender!!!</h1>;
 }
 
-export default Home
+export default Home;
